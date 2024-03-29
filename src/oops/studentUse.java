300e7b025cd061d1785fec9a7dc6d9b36bdd8bfe
Dwivedi Priya <pd7668665@gmail.com>
2024-03-29 18:46:55 +0530

    java

--- src/oops/studentUse.java
+++ src/oops/studentUse.java
@@ -1,15 +1,41 @@
 package oops;
 public class studentUse {
     public static void main(String[] args) {
-        // Creating new Student objects
-        Student student1 = new Student();
-        student1.name="Priya";
-        Student student2=new Student();
-        student2.name="Sachin";
-        System.out.println(student1.name);
+        //without using constructor
+        // StudentInfo student1 = new StudentInfo();
+        // student1.name="Priya";
+        // student1.setRollNo(51);
+        StudentInfo student1 = new StudentInfo("Priya",51);
+        StudentInfo student2 = new StudentInfo("Isha",99);
+        student1.print();
+        student2.print();
     }
 }
-class Student {
-    String name;
-    int age;
+
+class StudentInfo{
+     String name;
+    private int roll_no;
+
+    //Constructor
+    public StudentInfo(String n,int rn){
+        name=n;
+        roll_no=rn;
+    }
+
+    //access the private rollNo
+    public int getRollNo(){
+       return roll_no;
+    }
+
+    //change the  rollNo that is private
+    public void setRollNo(int rn){
+        roll_no=rn;
+    }
+
+    //printFunction
+    public void print(){
+        System.out.println(name+":"+roll_no);
+    }
 }
+
+
