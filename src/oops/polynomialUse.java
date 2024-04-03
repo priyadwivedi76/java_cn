c8bf282dd13e6058fe9d07c8b766479465deed23
Dwivedi Priya <pd7668665@gmail.com>
2024-04-03 22:51:21 +0530

    java

--- src/oops/polynomialUse.java
+++ src/oops/polynomialUse.java
@@ -21,7 +21,7 @@ public class polynomialUse {
         p2.print();
         p1.print();
 
-        Polynomial p2=new Polynomial();
+        Polynomial p3=new Polynomial.add(p1,p2);
         p3.print();
         p1.print();
         p2.print();
@@ -29,18 +29,109 @@ public class polynomialUse {
 }
 
 class Polynomial{
-    private DynamicArray coefficient[];
+    private DynamicArray coefficient;
     private int degree;
 
     //constructor
-    public Polynomial(){}
-
-    public void print(){
-        System.out.print("x^"+coefficient.length);
+    public Polynomial(){
+         coefficient=new DynamicArray();
     }
 
+    //getter
+    public int getCoefficient(int degree){
+        if(degree<this.coefficient.length){
+            return coefficient[degree];
+        }
+        return 0;
+    }
+
+    //setter
     public void setCoefficient(int degree,int coefficient){
-         
+        if(degree>=this.coefficient.length){
+            coefficient.restructure();
+        }
+        coefficient[degree]=coefficient;
     }
-    
+    //add
+    public Polynomial add(Polynomial p){
+        Polynomial ans=new Polynomial();
+        int p1=this.coefficient.length;
+        int p2=p.coefficient.length;
+        int len=Math.min(p1,p2);
+        int i;
+        for(i=0;i<=len;i++){
+            ans.setCoefficient(i, this.coefficient[i]+p.coefficient[i]);
+        }
+
+        while(i<p1){
+            ans.setCoefficient(i, this.coefficient[i]);
+        }
+
+        while(i<p2){
+            ans.setCoefficient(i, p.coefficient[i]);
+        }
+        return ans;
+    }
+
+    //subtract
+    public Polynomial subtract(Polynomial p){
+        Polynomial ans=new Polynomial();
+        int p1=this.coefficient.length;
+        int p2=p.coefficient.length;
+        int len=Math.min(p1,p2);
+        int i;
+        for(i=0;i<len;i++){
+            ans.setCoefficient(i, this.coefficient[i]-p.coefficient[i]);
+        }
+
+        while(i<p1){
+            ans.setCoefficient(i, this.coefficient[i]);
+            i++;
+        }
+
+        while(i<p2){
+            ans.setCoefficient(i, -p.coefficient[i]);
+            i++;
+        }
+        return ans;
+    }
+
+    //multiply
+    public Polynomial multiply(Polynomial p){
+        Polynomial ans=new Polynomial();
+        for(int i=0;i<this.coefficient.length;i++){
+            for(int j=0;j<p.coefficient.length;j++){
+                int termDeg=i+j;
+                int termCoefficient=this.coefficient[i]*p.coefficient[j];
+                int oldCoefficient=ans.getCoefficient(termDeg);
+                ans.setCoefficient(termDeg,termCoefficient+oldCoefficient);
+            }
+        }
+        return ans;
+    }
+
+    //take input of two polynomial and give the output of new plynomial
+    public static Polynomial add(Polynomial p1,Polynomial p2){
+        Polynomial ans=new Polynomial();
+        int p1=p1.coefficient.length;
+        int p2=p2.coefficient.length;
+        int len=Math.min(p1,p2);
+        int i;
+        for(i=0;i<=len;i++){
+            ans.setCoefficient(i, p1.coefficient[i]+p2.coefficient[i]);
+        }
+
+        while(i<=p1){
+            ans.setCoefficient(i, p1.coefficient[i]);
+            i++;
+        }
+
+        while(i<=p2){
+            ans.setCoefficient(i, p2.coefficient[i]);
+            i++;
+        }
+        return ans;
+    }
+
+
 }
