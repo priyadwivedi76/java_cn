14a7c01a0bc725f939c158988ca03e7009e6ee96
Dwivedi Priya <pd7668665@gmail.com>
2024-07-11 19:43:27 +0530

    java

--- src/simple/fibonacci.java
+++ src/simple/fibonacci.java
@@ -1,2 +1,26 @@
-package src.simple;public class fibonacci {
+package src.simple;
+
+import java.util.Scanner;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the order number of the fibonacci series");
+        int num= sc.nextInt();
+        int a=0;
+        int b=1;
+        int c;
+        int temp;
+        System.out.print(a+","+b+",");
+         if(num>2){
+            for(int i=2;i<num;i++){
+                c=a+b;
+                temp=a;
+                a=b;
+                b=c;
+                System.out.print(c+",");
+            }
+        }
+
+    }
 }
