ca92d00979a2d07b7b088b1ddffce4055836e07a
Dwivedi Priya <pd7668665@gmail.com>
2024-07-13 20:26:04 +0530

    java

---
+++ src/oops/fractionUse.java
@@ -0,0 +1,97 @@
+package src.oops;
+
+public class fractionUse {
+    public static void main(String[] args) {
+        Fraction f1=new Fraction(20,30);
+        f1.print();
+
+        f1.setNumerator(12);
+        int d=f1.getDenominator();
+        System.out.println(d);
+        f1.print();
+
+        f1.setNumerator(10);
+        f1.setDenominator(30);
+        f1.print();
+
+        Fraction f2=new Fraction(3,4);
+        f1.add(f2);
+        f1.print();
+        f2.print();
+
+        Fraction f3=new Fraction(4,5);
+        f3.multiply(f2);
+        f3.print();
+        f2.print();
+
+        Fraction f4=Fraction.add(f1,f3);
+        f1.print();
+        f3.print();
+        f4.print();
+    }
+}
+
+class Fraction{
+    private int numerator;
+    private int denominator;
+    public Fraction(int numerator,int denominator){
+        this.numerator=numerator;
+        if(denominator==0){
+            return;
+        }
+        this.denominator=denominator;
+        simplify();
+    }
+
+    private void simplify(){
+        int gcd = 1;
+        int small=Math.min(numerator,denominator);
+        for(int i=2;i<=small;i++){
+            if(numerator%i==0 && denominator%i==0){
+                gcd=i;
+            }
+        }
+        this.numerator=numerator/gcd;
+        this.denominator=denominator/gcd;
+    }
+
+    public void print(){
+        if(denominator==1){
+            System.out.println(numerator);
+        }else{
+            System.out.println(numerator+"/"+denominator);
+        }
+    }
+
+    public int getNumerator(){
+        return numerator;
+    }
+     public void setNumerator(int numerator){
+        this.numerator=numerator;
+        simplify();
+     }
+    public int getDenominator(){
+        return denominator;
+    }
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+        simplify();
+    }
+    public void add(Fraction f){
+        this.numerator=this.numerator*f.denominator+f.numerator*this.denominator;
+        this.denominator=this.denominator*f.denominator;
+        simplify();
+    }
+    public void multiply(Fraction f){
+        this.numerator=this.numerator*f.numerator;
+        this.denominator=this.denominator*f.denominator;
+        simplify();
+    }
+
+    public static Fraction add(Fraction f1,Fraction f2){
+        int newNumerator=f1.numerator*f2.denominator+f1.denominator*f2.numerator;
+        int newDenominator=f1.denominator*f2.denominator;
+        Fraction newFraction=new Fraction(newNumerator,newDenominator);
+        return newFraction;
+    }
+}
