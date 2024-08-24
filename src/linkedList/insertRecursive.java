615eeccc1134ba94969294cccbed85ad300baf43
Dwivedi Priya <pd7668665@gmail.com>
2024-08-24 12:41:00 +0530

    java

---
+++ src/linkedList/insertRecursive.java
@@ -0,0 +1,69 @@
+package linkedList;
+
+import java.util.Scanner;
+
+public class insertRecursive {
+    
+    //second method to printRecursive
+    public static void printRecursive(Node<Integer> head){
+        if(head==null){
+            return; 
+        }
+        System.out.print(head.data+" ");
+        printRecursive(head.next);
+    }
+
+    public static Node<Integer> insertR(Node<Integer> head,int element,int position){
+        if(position==0){
+            Node<Integer> elementNode=new Node<>(element);
+            elementNode.next=head;
+            return elementNode;
+        }else{
+            head.next=insertR(head.next, element, position-1);
+            return head;
+        }
+    }
+
+    //take input
+    public static Node<Integer> takeInput(){
+        Scanner sc=new Scanner(System.in);
+        int data=sc.nextInt();
+        Node<Integer> head=null,tail=null;
+
+        //data received in linked list if it is other than -1 if we get -1 stop the linkedlist
+        while(data != -1){
+            Node<Integer> currentNode=new Node<Integer>(data);
+            if(head == null){
+                //if head is empty put the number recieved into the head node 
+                head=currentNode;
+                tail=currentNode;
+            }
+            else{
+                //if head is not  empty connect  the next data to the tail to the head
+                tail.next=currentNode;
+                tail=tail.next;
+            }
+            //take the next data
+            data=sc.nextInt();
+        }
+        return head;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Enter the elements in linkedList");
+        Node<Integer> head=takeInput();
+        printRecursive(head);
+        System.out.println("After insertion:");
+        head=insertR(head, 20, 0);
+        printRecursive(head);
+    }
+}
+
+//class to create node
+class Node<T>{
+    T data;
+    Node<T> next;
+    public Node(T data){
+        this.data=data;
+    }
+}
