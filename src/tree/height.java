31c9649addc972347f93ad325ecf1de134866f9b
Dwivedi Priya <pd7668665@gmail.com>
2024-10-16 21:35:09 +0530

    java

---
+++ src/tree/height.java
@@ -0,0 +1,147 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class height {
+    public static void printTree(Tree<Integer> root){
+        String s=root.data+" ";
+        //print root data and its children
+        for(int i=0;i<root.children.size();i++){
+            s=s+root.children.get(i).data+",";
+        }
+        System.out.println(s);
+    
+        //print the children of the root
+        for(int i=0;i<root.children.size();i++){
+            printTree(root.children.get(i));
+        }
+    }
+
+    public static Tree<Integer> inputLevelWise() throws EmptyQueue{
+        Scanner sc=new Scanner(System.in);
+        Queue<Tree<Integer>> pendinNode=new Queue<>();
+        System.out.println("Enter the root node");
+        int rootData=sc.nextInt();
+        Tree<Integer> root=new Tree<Integer>(rootData);
+        pendinNode.enqueue(root);
+        while(!pendinNode.isEmpty()){
+            try{
+                Tree<Integer> frontNode=pendinNode.dequeue();
+                System.out.println("Enter the number of children of "+frontNode.data);
+                int childCount=sc.nextInt();
+                for(int i=0;i<childCount;i++){
+                    System.out.println("Enter the "+(i+1)+" child of the node"+frontNode.data);
+                    int childData=sc.nextInt();
+                    Tree<Integer> childNode=new Tree<>(childData);
+                    frontNode.children.add(childNode);
+                    pendinNode.enqueue(childNode);
+                }
+            }catch(Exception e){
+                throw new EmptyQueue();
+            }
+        }
+        return root;
+    }
+
+    public static int height(Tree<Integer> root){
+        if(root==null){
+            return 0;
+        }
+        int height=0;
+        for(int i=0;i<root.children.size();i++){
+            height=Math.max(height,height(root.children.get(i)));
+        }
+        return height+1;
+    }
+
+    public static void main(String[] args) throws EmptyQueue{
+        Tree<Integer> root=inputLevelWise();
+        printTree(root);
+        int height=height(root);
+        System.out.println("The height of the tree is:"+height);
+    }
+}
+
+
+class Tree<T>{
+    T data;
+    ArrayList<Tree<T>> children;
+    public Tree(T data){
+        this.data=data;
+        children = new ArrayList<>();
+    }
+}
+
+class Queue<T>{
+    private Node<T> front;
+    private Node<T> rear;
+    private int size;
+
+    //constructor
+    public Queue(){
+        front=null;
+        rear=null;
+        size=0;
+    }
+
+    public void enqueue(T elem){
+        Node<T> newNode=new Node<>(elem);
+        if(front==null && rear==null){
+            front=newNode;
+            rear=newNode;
+        }else{
+            rear.next=newNode;
+            rear=newNode;
+        }
+        size++;
+    }
+
+    public T dequeue() throws EmptyQueue{
+        if(front==null && rear==null){
+            throw new EmptyQueue();
+        }
+        T temp=front.data;
+        front=front.next;
+        if(front==null){
+            rear=null;
+        }
+        size--;
+        return temp;
+    }
+    
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+    public T front() throws EmptyQueue{
+        if (isEmpty()) {
+            throw new EmptyQueue();
+        }
+        return front.data;
+    }
+
+    public T rear() throws EmptyQueue{
+        if (isEmpty()) {
+            throw new EmptyQueue();
+        }
+        return rear.data;
+    }
+}
+
+class Node<T>{
+    T data;
+    Node<T> next;
+    public Node(T data){
+        this.data=data;
+    }
+}
+
+//custom exception class to handle empty queue exception
+class EmptyQueue extends Exception{
+    public String getMessage(){
+        return "Queue is empty";
+    }
+}
